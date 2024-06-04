5f4a765a1f8b067304a31428371a20ffdc2623a2
tonyG <tx.tony9@gmail.com>
2024-06-04 11:53:26 -0700

    added a main container class and cards within that class

--- Gemini Clone/src/components/Main/Main.jsx
+++ Gemini Clone/src/components/Main/Main.jsx
@@ -9,6 +9,30 @@ const Main = () => {
         <p>Gemini</p>
         <img src={assets.user_icon} alt="" />
       </div>
+      <div className="main-container">
+        <div className="great">
+          <p><span>Hello, Dev.</span></p>
+          <p>How can I help you today?</p>
+        </div>
+        <div className="cards">
+          <div className="card">
+            <p>Suggest beautiful place to travel to</p>
+            <img src={assets.compass_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Briefly summarize this concept: urban planning</p>
+            <img src={assets.bulb_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Brainstorm team building activities for work reatreat</p>
+            <img src={assets.message_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Improve readability of the following code</p>
+            <img src={assets.code_icon} alt="" />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
