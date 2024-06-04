d5c2726d158ce9a2321745b6791908d56c066bd1
tonyG <tx.tony9@gmail.com>
2024-06-04 12:18:29 -0700

    added new styleas to card class in main.css  file and changed the project name to ALIS

--- Gemini Clone/src/components/Main/Main.jsx
+++ Gemini Clone/src/components/Main/Main.jsx
@@ -6,7 +6,7 @@ const Main = () => {
   return (
     <div className='main'>
       <div className="nav">
-        <p>Gemini</p>
+        <p>ALIS</p>
         <img src={assets.user_icon} alt="" />
       </div>
       <div className="main-container">
