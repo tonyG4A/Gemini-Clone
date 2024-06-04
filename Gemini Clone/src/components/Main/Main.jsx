daae81a55b4bc6df7cdf7b389173b6d513db2e86
tonyG <tx.tony9@gmail.com>
2024-06-03 23:26:20 -0700

    added classes and styles to main.jsk and main.css

--- Gemini Clone/src/components/Main/Main.jsx
+++ Gemini Clone/src/components/Main/Main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import './Main.css'
+import { assets } from '../../assets/assets'
 
 const Main = () => {
   return (
-    <div>
-      
+    <div className='main'>
+      <div className="nav">
+        <p>Gemini</p>
+        <img src={assets.user_icon} alt="" />
+      </div>
     </div>
   )
 }
